cec10326fdf88819f619303fcfd8b809de8c2c0a
ceenobi <cobimbachu@gmail.com>
2022-09-26 08:27:13 +0100

    eslint disable

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     domains: ['res.cloudinary.com'],
     deviceSizes: [320, 768, 960, 1200, 1536],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
